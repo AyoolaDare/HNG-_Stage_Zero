847826afeb2175c77db17eef2bea71fe120deb40
owala1 <owalacore@gmail.com>
2024-06-28 18:43:22 +0100

    My first Commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('change-text-button').addEventListener('click', function() {
+    document.getElementById('welcome-text').innerText = 'Hello! Thanks for visiting my project!';
+});
